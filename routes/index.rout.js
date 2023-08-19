4198880f84c5ba16e0508adecdb36c8805c2b4b9
joewilliam21 <youssef2103603@miuegypt.edu.eg>
2023-08-19 07:13:18 +0300

    frontend

--- routes/index.rout.js
+++ routes/index.rout.js
@@ -5,5 +5,18 @@ router.get('/', function (req, res, next) {
   console.log('index.js: GET /');
   res.render('pages/Home',{ user: (req.session.user === undefined ? "" : req.session.user)});
 });
+router.get('/about', function (req, res, next) {
+  console.log('index.js: GET /');
+  res.render('pages/about',{ user: (req.session.user === undefined ? "" : req.session.user)});
+});
+router.get('/products', function (req, res, next) {
+  console.log('index.js: GET /');
+  res.render('pages/products',{ user: (req.session.user === undefined ? "" : req.session.user)});
+});
+router.get('/singleprodct', function (req, res, next) {
+  console.log('index.js: GET /');
+  res.render('pages/single_product',{ user: (req.session.user === undefined ? "" : req.session.user)});
+});
+
 
 export default router;
